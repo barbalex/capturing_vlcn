e55811f0f026b94d2b4fd76d849b7e46d8f7f7de
Alexander Gabriel <alex.barbalex@gmail.com>
2023-07-07 14:15:31 +0200

    init

---
+++ src/schemas/main.mjs
@@ -0,0 +1,19 @@
+/**
+ * Note: modifying the schema currently requires:
+ * 1. Restarting the server
+ * 2. Refreshing the browser
+ *
+ * Other than that, DB migrations are automatically handled.
+ * WARNING: not null without default value is not supported
+ */
+export default {
+  namespace: "default",
+  name: "main",
+  active: true,
+  content: /*sql*/ `
+    CREATE TABLE IF NOT EXISTS projects (id TEXT NOT NULL PRIMARY KEY, account_id TEXT, name TEXT, label TEXT, crs INTEGER DEFAULT 4326, deleted INTEGER DEFAULT 0, use_labels INTEGER DEFAULT 0);
+    SELECT crsql_as_crr('projects');
+  `,
+};
+// CREATE TABLE IF NOT EXISTS projects (id TEXT NOT NULL PRIMARY KEY, account_id TEXT NOT NULL, name TEXT, label TEXT, crs INTEGER DEFAULT 4326, deleted INTEGER DEFAULT 0, use_labels INTEGER DEFAULT 0);
+// SELECT crsql_as_crr('projects');
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    target: "esnext",
+  },
+  optimizeDeps: {
+    exclude: ["@vite/client", "@vite/env"],
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        globPatterns: [
+          "**/*.js",
+          "**/*.css",
+          "**/*.svg",
+          "**/*.html",
+          "**/*.png",
+          "**/*.wasm",
+        ],
+      },
+    }),
+  ],
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+});
