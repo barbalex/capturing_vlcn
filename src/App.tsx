6147552052b00b61d01f5e922655bb10d1472f84
Alexander Gabriel <alex.barbalex@gmail.com>
2023-07-09 19:25:08 +0200

    remove nanoid

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import { useCachedState, useQuery } from "@vlcn.io/react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import vlcnLogo from "./assets/vlcn.png";
-import "./App.css";
-import randomWords from "./support/randomWords.js";
-import { DBAsync } from "@vlcn.io/xplat-api";
-import { useDB } from "@vlcn.io/react";
+import { useCachedState, useQuery } from '@vlcn.io/react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import vlcnLogo from './assets/vlcn.png'
+import './App.css'
+import randomWords from './support/randomWords.js'
+import { DBAsync } from '@vlcn.io/xplat-api'
+import { useDB } from '@vlcn.io/react'
+import { uuidv4 } from 'uuidv7'
 
-type TestRecord = { id: string; name: string };
-const wordOptions = { exactly: 3, join: " " };
+type TestRecord = { id: string; name: string }
+const wordOptions = { exactly: 3, join: ' ' }
 
 function App({ dbid }: { dbid: string }) {
-  const ctx = useDB(dbid);
+  const ctx = useDB(dbid)
   const data = useQuery<TestRecord>(
     ctx,
-    "SELECT * FROM projects ORDER BY id DESC"
-  ).data;
+    'SELECT * FROM projects ORDER BY id DESC',
+  ).data
 
   const addData = () => {
-    ctx.db.exec("INSERT INTO projects (id, name) VALUES (?, ?);", [
-      nanoid(10),
+    ctx.db.exec('INSERT INTO projects (id, name) VALUES (?, ?);', [
+      uuidv4(),
       randomWords(wordOptions) as string,
-    ]);
-  };
+    ])
+  }
 
   const dropData = () => {
-    ctx.db.exec("DELETE FROM projects;");
-  };
+    ctx.db.exec('DELETE FROM projects;')
+  }
 
   return (
     <>
@@ -43,7 +44,7 @@ function App({ dbid }: { dbid: string }) {
       </div>
       <h1>Vite + React + Vulcan</h1>
       <div className="card">
-        <button onClick={addData} style={{ marginRight: "1em" }}>
+        <button onClick={addData} style={{ marginRight: '1em' }}>
           Add Data
         </button>
         <button onClick={dropData}>Drop Data</button>
@@ -69,10 +70,10 @@ function App({ dbid }: { dbid: string }) {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <p>
-          Open another browser and navigate to{" "}
+          Open another browser and navigate to{' '}
           <a href={window.location.href} target="_blank">
             this window's url
-          </a>{" "}
+          </a>{' '}
           to test sync.
         </p>
       </div>
@@ -80,7 +81,7 @@ function App({ dbid }: { dbid: string }) {
         Click on the Vite, React and Vulcan logos to learn more
       </p>
     </>
-  );
+  )
 }
 
 function EditableItem({
@@ -88,9 +89,9 @@ function EditableItem({
   id,
   value,
 }: {
-  db: DBAsync;
-  id: string;
-  value: string;
+  db: DBAsync
+  id: string
+  value: string
 }) {
   // Generally you will not need to use `useCachedState`. It is only required for highly interactive components
   // that write to the database on every interaction (e.g., keystroke or drag) or in cases where you want
@@ -99,33 +100,17 @@ function EditableItem({
   // `useCachedState` will never be required once when one of the following is true:
   // a. We complete the synchronous Reactive SQL layer (SQLiteRX)
   // b. We figure out how to get SQLite-WASM to do a write + read round-trip in a single event loop tick
-  const [cachedValue, setCachedValue] = useCachedState(value);
+  const [cachedValue, setCachedValue] = useCachedState(value)
   const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCachedValue(e.target.value);
+    setCachedValue(e.target.value)
     // You could de-bounce your write to the DB here if so desired.
-    return db.exec("UPDATE projects SET name = ? WHERE id = ?;", [
+    return db.exec('UPDATE projects SET name = ? WHERE id = ?;', [
       e.target.value,
       id,
-    ]);
-  };
+    ])
+  }
 
-  return <input type="text" value={cachedValue} onChange={onChange} />;
+  return <input type="text" value={cachedValue} onChange={onChange} />
 }
 
-export default App;
-
-const nanoid = (t = 21) =>
-  crypto
-    .getRandomValues(new Uint8Array(t))
-    .reduce(
-      (t, e) =>
-        (t +=
-          (e &= 63) < 36
-            ? e.toString(36)
-            : e < 62
-            ? (e - 26).toString(36).toUpperCase()
-            : e > 62
-            ? "-"
-            : "_"),
-      ""
-    );
+export default App
