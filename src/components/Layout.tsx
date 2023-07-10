2962571b6d5d64f281fbada29b400cfba5f884ed
Alexander Gabriel <alex.barbalex@gmail.com>
2023-07-10 15:48:57 +0200

    refactor

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,16 @@
 import styled from '@emotion/styled'
 import { Outlet } from 'react-router-dom'
 
-import Header from './Header'
+import { Header } from './Header'
 
 const Container = styled.div`
   height: 100%;
   width: 100%;
 `
 
-const Layout = (): React.FC => (
+export const Layout = (): React.FC => (
   <Container>
     <Header />
     <Outlet />
   </Container>
 )
-
-export default Layout
